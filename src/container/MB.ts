f1cea4d4770f02b4b6e78c4cfabcad70618af4aa
alexander.guesnon <alexander.guesnon@gmail.com>
2020-03-19 21:56:17 -0700

    ya classes

--- src/container/MB.ts
+++ src/container/MB.ts
@@ -1,8 +1,28 @@
+
+class Button {
+  button: string
+  constructor(icon: string){
+    this.button = icon
+  }
+}
+class ControlPanel {
+    ControlPanel: Button[]
+    constructor(panel: Button[]) {
+        this.ControlPanel = panel
+    }
+
+}
+
+const icons = ['♻️','💤','➕','➖','⬆️','➡️','⬇️','⬅️']
+const buttons = icons.map( icon => new Button(icon) )
+
 const state = {
-  title: "Mandelbrot Set",
+  title: buttons,
+  ControlPanel(){ return buttons },
 }
 const getters = {
-    'title': ( state, getters) => { return state.title  }
+    title: ( state, getters) => {  return state.title  },
+    buttons: (state, getters) => { return state.ControlPanel }
   }
 
 const mutations = {
@@ -12,6 +32,7 @@ const mutations = {
 const actions = {
 
 }
+
 
 export default {
   state,
