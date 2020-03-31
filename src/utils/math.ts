2ffd0cddf468d342d769565e18b25c1b7282bdab
alex gooz <alexander.guesnon@gmail.com>
2020-03-30 21:56:32 -0700

    simple add

--- src/utils/math.ts
+++ src/utils/math.ts
@@ -1,3 +1,5 @@
+// i^2 = -1
+// a+bi
 class complexNumbers {
   real: number
   imaginary: number
@@ -5,5 +7,11 @@ class complexNumbers {
   constructor (real:number, imginary:number){
     this.real = real
     this.imaginary = imginary
+  }
+  
+  add(temp:complexNumbers){
+    const I = this.imaginary + temp.imaginary
+    const R = this.real + temp.real
+    return new complexNumbers(R,I)
   }
 }
