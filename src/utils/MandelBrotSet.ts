db057bf5c2e83cdcc0464649868478709ce4d5b7
alex gooz <alexander.guesnon@gmail.com>
2020-04-04 11:00:40 -0700

    rough mb class util

--- src/utils/MandelBrotSet.ts
+++ src/utils/MandelBrotSet.ts
@@ -1,5 +1,12 @@
 import complexNumbers from './complex-num'
 
+/**
+ * @class axis
+ * this class is to consolidate
+ * all axis vars and functionss
+ * 
+ * i.e. its beeter looking i guess
+ */
 export class axis {
   min:number
   max:number
@@ -13,12 +20,15 @@ export class axis {
   transform(location:number){
     const axisLength = Math.abs(this.min) + Math.abs(this.max)
     const pixleLength =  axisLength / this.pixels
-
     return pixleLength
   }
 
 }
 
+/**
+ * @class Mandelbrot_set
+ * main class
+ */
 class Mandelbrot_set {
   maxIterations:number
   x:axis
@@ -30,29 +40,36 @@ class Mandelbrot_set {
     this.y = y !== undefined ? y : new axis(-1, 1, 200)
     }
 
-  private MBequation(n:complexNumbers){
+  private MBequation(c:complexNumbers){
+    const n = new complexNumbers(0,0)
     const square = n.multiply(n)
-    return square.add(new complexNumbers(1,0))
+    return square.add(c)
   }
 
   private calc(x:number, y:number){
     let MBout = new complexNumbers(x,y)
-    for (let i = 0; i < this.maxIterations; i++) {
+    let i = 0
+    for (; i < this.maxIterations; i++) {
       MBout = this.MBequation(MBout)
-      if(false){
-        break
+      if(Math.abs(MBout.real) <= 2){
+        return 0
+      }else if(MBout.real >= 0){
+        return i
       }
     }
+    return i
   }
-
 
   output(){
     const canvas = [[this.x.pixels],[this.y.pixels]]
-    
-    
-
+    for (let i = 0; i < canvas.length; i++) {
+      for (let j = 0; j < canvas[i].length; j++) {
+        canvas[i][j] = this.calc(this.x.transform(i), this.y.transform(j))
+      }
+    }
     return canvas
   }
 
 }
 
+export default Mandelbrot_set
