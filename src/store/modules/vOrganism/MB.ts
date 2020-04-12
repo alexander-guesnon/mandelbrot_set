ed3a011b0f8264ea67b3305ef27545354722feb9
alexander-guesnon <alexander.guesnon@gmail.com>
2020-04-12 16:44:17 -0700

    plan

--- src/store/modules/vOrganism/MB.ts
+++ src/store/modules/vOrganism/MB.ts
@@ -2,7 +2,7 @@ import Controller from '../vMolecule/controller'
 import Image from '../vAtom/image'
 
 const state = {
-
+// this will be all the atrabutest of the aculke mb all blow are abstractions
 }
 const getters = {
    
