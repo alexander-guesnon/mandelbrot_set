93941afb41c25ba344755abd8d95d5f87cda1075
alexander-guesnon <alexander.guesnon@gmail.com>
2020-04-15 10:40:25 -0700

    state

--- src/components/vOrganism/store/MB.ts
+++ src/components/vOrganism/store/MB.ts
@@ -2,24 +2,13 @@ import Controller from '../vMolecule/store/controller'
 import Image from '../vMolecule/vAtom/store/image'
 
 const state = {
-title: 'Mandle Brotset'
+title: 'Mandelbrot Set'
 }
-const getters = {
-   
-  }
 
-const mutations = {
- 
-  }
-
-  const actions = {}
 
 
 export default {
   state,
-  mutations,
-  actions,
-  getters,
   modules: {
     Controller,
     Image
