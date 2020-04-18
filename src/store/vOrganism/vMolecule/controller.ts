a427c3a1cef601ae35d9c5ceb4f38f73f04491f2
alexander-guesnon <alexander.guesnon@gmail.com>
2020-04-18 15:59:24 -0700

    console log

--- src/store/vOrganism/vMolecule/controller.ts
+++ src/store/vOrganism/vMolecule/controller.ts
@@ -37,15 +37,15 @@ const actions = {
 };
 
 const mutations = {
-  [M.DOWN]() {},
-  [M.LEFT]() {},
-  [M.REFRESH]() {},
-  [M.RESTART]() {},
-  [M.RIGHT]() {},
-  [M.SLEEP]() {},
-  [M.UP]() {},
-  [M.ZOOM_IN]() {},
-  [M.ZOOM_OUT]() {},
+  [M.DOWN]() {console.log("bam")},
+  [M.LEFT]() {console.log("bam")},
+  [M.REFRESH]() {console.log("bam")},
+  [M.RESTART]() {console.log("bam")},
+  [M.RIGHT]() {console.log("bam")},
+  [M.SLEEP]() {console.log("bam")},
+  [M.UP]() {console.log("bam")},
+  [M.ZOOM_IN]() {console.log("bam")},
+  [M.ZOOM_OUT]() {console.log("bam")},
 };
 
 export default {
