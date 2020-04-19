13f04f9184629b104187424735f15f5c8e1b13b2
alexander-guesnon <alexander.guesnon@gmail.com>
2020-04-19 10:35:27 -0700

    reorgnisation:
    kudos to this guy: https://github.com/pocka/vue-container-component-example

--- src/store/modules/vAtom/button.ts
+++
@@ -1,9 +0,0 @@
-//import VButton from '../../components/vAtom/vButton.vue'
-export default class Button {
-    icon:string
-    action:string
-    constructor (icon:string, action:string) {
-        this.icon = icon
-        this.action = action
-    }
-}
